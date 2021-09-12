d4cf8353c5943a32048c7b8cb33ed2782a286788
Jerry <songnman@gmail.com>
2021-09-13 08:30:28 +0900

    루아 체킹

--- check_lua.py
+++ check_lua.py
@@ -1,22 +1,59 @@
-from slpp import slpp as lua
 import luadata
+import re
+import csv
+import pandas as pd
 
-code = open("NKM_UNIT_BARCODE_C_SISTER.txt",'r', encoding='utf-8-sig')
+
+path = "NKM_UNIT_BARCODE_C_SISTER.txt"
+code = open(path,'r', encoding='utf-8-sig')
 lines = code.read()
+lines = re.sub('--.+', '', lines)
 lines = lines.replace(' ','')
 lines = lines.replace('\t','')
-# lines = lines.replace('\n',' ')
-# lines = lines.split(',')
-# for line in lines:
-	# print(line)
-
-
+lines = lines.replace('\n','')
+lines = lines.replace('/','/')
 code.close()
-print(lines)
-# print(type(lines))
-# print(type(lua.decode(lines)))
-# print(lua.decode(lines))
-# data = luadata.write("NKM_UNIT_BARCODE_C_SISTER.txt",'r', encoding="utf-8", indent="\t", prefix="return ")
 
-# data = luadata.read("NKM_UNIT_BARCODE_C_SISTER.txt", encoding="utf-8")
-luadata.unserialize(code, encoding="utf-8", multival=False)
+# print(lines)
+data = luadata.unserialize(lines,encoding='utf-8-sig')
+# print(data)
+df1 = {}
+df2 = {}
+df3 = {}
+f = open('TEST1.csv','w', newline='')
+writer = csv.writer(f)
+for k, v in data.items():
+	if isinstance(v, list):
+		for item in v:
+			for k, v in item.items():
+				# if isinstance(v, list):
+	# 				for item in v:
+	# 					for k, v in item.items():
+	# 						writer.writerow([k, v])
+	# 						df3.update({k:v})
+	# 			else:
+					writer.writerow([item, k, v])
+					df2.update({k:v})
+	else:
+		writer.writerow([k, v])
+		df1.update({k:v})
+		
+		
+
+# dict1 = data
+# df = pd.DataFrame(data=dict1, index=[0])
+# df = (df.T)
+# print (df)
+# df.to_excel('dict1.xlsx')
+
+df1 = pd.DataFrame.from_dict(df1, orient='index')
+df2 = pd.DataFrame.from_dict(df2, orient='index')
+df3 = pd.DataFrame.from_dict(df3, orient='index')
+
+
+writer = pd.ExcelWriter("LUAsheet.xlsx", engine = 'xlsxwriter')
+df1.to_excel(writer, sheet_name = 'x1')
+df2.to_excel(writer, sheet_name = 'x2')
+df3.to_excel(writer, sheet_name = 'x3')
+writer.save()
+writer.close()
