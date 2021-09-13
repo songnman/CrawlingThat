8fecfe6aab7ac9763e6c9ab3642b2e97c0ba2d77
Jerry <songnman@gmail.com>
2021-09-13 19:08:11 +0900

    루아 체커 디벨롭중

--- check_lua.py
+++ check_lua.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 
 path = "NKM_UNIT_BARCODE_C_SISTER.txt"
+path = "NKM_UNIT_CA_JOO_SHI_YOON.txt"
 code = open(path,'r', encoding='utf-8-sig')
 lines = code.read()
 lines = re.sub('--.+', '', lines)
@@ -25,20 +26,21 @@ writer = csv.writer(f)
 for k, v in data.items():
 	if isinstance(v, list):
 		for item in v:
-			for k, v in item.items():
-				# if isinstance(v, list):
-	# 				for item in v:
-	# 					for k, v in item.items():
-	# 						writer.writerow([k, v])
-	# 						df3.update({k:v})
-	# 			else:
-					writer.writerow([item, k, v])
-					df2.update({k:v})
+			for i, j in item.items():
+				if isinstance(j, list):
+					for item in j:
+						for x, y in item.items():
+								# print(type(y))
+							if isinstance(y, list):
+								for item in y:
+									print(type(item))
+									writer.writerow([k,i,x,item])
+							else:
+								writer.writerow([k,i,x,y])
+				else:
+					writer.writerow([k,i,j,None])
 	else:
-		writer.writerow([k, v])
-		df1.update({k:v})
-		
-		
+		writer.writerow([k,v,None,None])
 
 # dict1 = data
 # df = pd.DataFrame(data=dict1, index=[0])
@@ -56,4 +58,3 @@ df1.to_excel(writer, sheet_name = 'x1')
 df2.to_excel(writer, sheet_name = 'x2')
 df3.to_excel(writer, sheet_name = 'x3')
 writer.save()
-writer.close()
