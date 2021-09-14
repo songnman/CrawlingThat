d5a9dd690dab0d85e311a6a3ed6a8aea3149c95f
Jerry <songnman@gmail.com>
2021-09-14 12:52:31 +0900

    루아체커 컬럼 정리

--- check_lua.py
+++ check_lua.py
@@ -3,60 +3,59 @@ import re
 import csv
 import pandas as pd
 
+def LoadData(path):
+	code = open(path,'r', encoding='utf-8-sig')
+	lines = code.read()
+	lines = re.sub('--.+', '', lines)
+	lines = lines.replace(' ','')
+	lines = lines.replace('\t','')
+	lines = lines.replace('\n','')
+	lines = lines.replace('/','/')
+	code.close()
+	data = luadata.unserialize(lines,encoding='utf-8-sig')
+	ExtractUnitTemplet(data)
 
-path = "NKM_UNIT_BARCODE_C_SISTER.txt"
-path = "NKM_UNIT_CA_JOO_SHI_YOON.txt"
-code = open(path,'r', encoding='utf-8-sig')
-lines = code.read()
-lines = re.sub('--.+', '', lines)
-lines = lines.replace(' ','')
-lines = lines.replace('\t','')
-lines = lines.replace('\n','')
-lines = lines.replace('/','/')
-code.close()
-# print(lines)
-data = luadata.unserialize(lines,encoding='utf-8-sig')
-# print(data)
+def ExtractUnitTemplet(data):
+	f = open('TEST1.csv','w', newline='')
+	writer = csv.writer(f)
+	StateSet_List = ["m_listAttackStateData","m_listSkillStateData","m_listHyperSkillStateData","m_listHitCriticalFeedBack"]
+	StateInfo_List = ["m_StateName"]
+	StateInfo_Avoid_List = ["m_NKM_UNIT_STATE_TYPE"]
+	state_index = 0
+	# for k,v in data:
+	# 	if v["StataName"] == "StateName": break
+	for k, v in data.items():
+		if isinstance(v, list):
+			for item in v: #?리스트니까 For문 돌려야됨
+				for i, j in item.items():
+					list_count = 0
+					if isinstance(j, list): #?리스트일 경우 스테이트 이벤트
+						tag = "StateEvent"
+						list_count = len(j)
+					elif k in StateSet_List: #?스테이트묶음 종류에 포함될 경우 스테이트묶음
+						tag = "StateSet"
+					else: #?나머지는 전부 스테이트 정보
+						tag = "StateInfo"
+					if i in StateInfo_List and k not in StateSet_List : state_index += 1 #! 인포셋에 걸리면 스테이트인덱스 증가
+					if i in StateInfo_Avoid_List and k not in StateSet_List : continue
+					writer.writerow([tag,k,i,j,list_count,state_index])
+		else:
+			tag = "BasicInfo"
+			writer.writerow([tag,None,k,v,None,state_index]) #?유닛 정보
+
+
+
+# NKM_UNIT_CA_JOO_SHI_YOON.txt
+# NKM_UNIT_BARCODE_C_SISTER.txt
+LoadData("NKM_UNIT_CA_JOO_SHI_YOON.txt")
+
+
 df1 = {}
 df2 = {}
 df3 = {}
-f = open('TEST1.csv','w', newline='')
-writer = csv.writer(f)
-global StateName
-StateName = "USN_START"
-for k, v in data.items():
-	if isinstance(v, list):
-		for item in v:
-			for i, j in item.items():
-				if isinstance(j, list):
-					for item in j:
-						for x, y in item.items():
-							if isinstance(y, list):
-								for item in y:
-									writer.writerow([k,None,i,x,item])
-							elif isinstance(y, dict):
-								for a, b in y.items():
-									writer.writerow([k,i,x,a,b])
-							else:
-								writer.writerow([k,None,i,x,y])
-				elif isinstance(j, dict):
-					for a, b in j.items():
-						writer.writerow([k,None,i,a,b])
-				else:
-					writer.writerow([k,None,None,i,j])
-	else:
-		writer.writerow([None,None,None,k,v])
-
-# dict1 = data
-# df = pd.DataFrame(data=dict1, index=[0])
-# df = (df.T)
-# print (df)
-# df.to_excel('dict1.xlsx')
-
 df1 = pd.DataFrame.from_dict(df1, orient='index')
 df2 = pd.DataFrame.from_dict(df2, orient='index')
 df3 = pd.DataFrame.from_dict(df3, orient='index')
-
 
 writer = pd.ExcelWriter("LUAsheet.xlsx", engine = 'xlsxwriter')
 df1.to_excel(writer, sheet_name = 'x1')
