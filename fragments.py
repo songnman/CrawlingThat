901bbb462047e54727c4cc8d51a0b7f8d4ac79b7
Jerry <songnman@gmail.com>
2021-12-09 12:22:47 +0900

    색인 제외 리스트 생성 검색 랜덤값을 1초 ~ 10초로 변경

--- fragments.py
+++ fragments.py
@@ -6,8 +6,7 @@ import re
 import csv
 
 csv_list = [join('results\\',f) for f in listdir('results\\') ]
-# print(csv_list)
-
+csv_list = ['results\\All_result.csv']
 
 combined_csv = pd.concat([pd.read_csv(f) for f in csv_list ])
 title_list = combined_csv['title'].tolist()
@@ -24,11 +23,11 @@ for item in comment_dict:
 	for i in item:
 		comment_list.append(i['comment'])
 
-# all_list = title_list + content_list2 + comment_list
-all_list = title_list
+all_list = title_list + content_list2 + comment_list
+# all_list = title_list
 
 result_noun_list = ''.join(all_list) #*스트링 하나로 결합
-result_noun_list = re.sub("\!|\'|\?|\)|\(|\.", "", result_noun_list) #*결합된 스트링에서 특정 문자 제외
+result_noun_list = re.sub("\!|\'|\?|\)|\(||\.|(emoticon)", "", result_noun_list) #*결합된 스트링에서 특정 문자 제외
 result = result_noun_list #*제외된 문자로 적용
 
 result_noun_list = result_noun_list.split(' ') #* 스트링을 다시 LIST로 결합
@@ -40,5 +39,16 @@ if os.path.exists(my_file):
 f = open(my_file,'w', newline='', encoding='utf-8-sig')
 writer = csv.writer(f)
 
+deny_list = []
+list_file = "Deny_List.csv"
+if os.path.exists(list_file):
+	import csv
+	with open(list_file,'r', encoding='utf-8-sig', newline='') as csvfile:
+		rdr = csv.reader(csvfile)
+		for line in rdr:
+			deny_list.append(line[0])
+
+writer.writerow(["Keyword","count"])
 for x in range(len(result_noun_list)):
+	if(result_noun_list[x] in deny_list or len(result_noun_list[x]) < 2 ):continue
 	writer.writerow([result_noun_list[x],result.count(result_noun_list[x])])
